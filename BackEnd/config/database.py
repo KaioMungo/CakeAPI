9a03305efae60e0c20c2e17c9bb74582e395da15
Kaio Mungo <kaiao2006@gmail.com>
2025-10-07 18:23:58 -0300

    feat: file config added

---
+++ BackEnd/config/database.py
@@ -0,0 +1,14 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['HOST'] = '0.0.0.0'
+app.config['PORT'] = 5001
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
